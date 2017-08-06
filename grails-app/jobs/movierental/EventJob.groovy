a2c938e4900dbc9dc4d45ef2828a983f5513e808
Hubert <hutatar@gmail.com>
2017-08-06 21:49:42 +0200

    Sse from quartz job - wip

---
+++ grails-app/jobs/movierental/EventJob.groovy
@@ -0,0 +1,21 @@
+package movierental.events
+
+import grails.events.annotation.Publisher
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class EventJob {
+
+    static final String JOB_GENERATED_EVENT = "JOB_GENERATED_EVENT"
+    Random rand = new Random()
+
+    static triggers = {
+        simple name: 'oneSecTrigger', startDelay: 1000, repeatInterval: 1000
+    }
+
+    @Publisher('sseevent')
+    def execute() {
+        log.info("Executing")
+        rand.nextInt(100)
+    }
+}
